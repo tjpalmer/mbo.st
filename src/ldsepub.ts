9871f708d786bbcb28cf2316d2298ef0446ceed2
Tom <tjpalmer@tjpalmer.com>
2017-10-22 08:48:51 -0700

    Build volume

--- src/ldsepub.ts
+++ src/ldsepub.ts
@@ -1,25 +1,35 @@
 import {load} from 'cheerio';
+import {Chapter, Doc, Paragraph, Verse, Volume} from 'scripturetrack/src/text';
 
-export function epubParse(name: string) {
+export function epubParse(name: string): Volume{
   let EPub = require('epub');
   let epub = new EPub(name);
-  let parseChapter = (chapterId: string, docId: string) => {
+  let docMap = new Map<string, Doc>();
+  let docs: Doc[] = [];
+  let parseChapter = (chapterId: string, doc: Doc) => {
     epub.getChapter(chapterId, (error: any, text: string) => {
       let $ = load(text);
       let title = $('.title').attr('title');
       if (title) {
-        console.log(docId, 'Title: ', title);
+        doc.title = title;
       }
       // console.log(text);
-      let chapterNumber = +$('.titleNumber').text().split(/\s+/).slice(-1)[0];
-      console.log('Chapter', chapterNumber);
+      let verses: Verse[] = [];
       $('.verse-first, .verse').each((index, element) => {
         let $element = $(element);
         let $number = $element.find('.verseNumber');
         $number.remove();
-        let verseNumber = +$number.text().trim();
-        // console.log(verseNumber, $(element).text());
+        let number = +$number.text().trim();
+        let text = $(element).text();
+        verses.push({number, text});
       });
+      let number = +$('.titleNumber').text().split(/\s+/).slice(-1)[0];
+      let paragraphs: Paragraph[] = verses.map(verse => {
+        return {size: verse.text.length, verses: [verse]};
+      });
+      let size = sum(paragraphs.map(paragraph => paragraph.size + 1));
+      let chapter: Chapter = {number, paragraphs, size};
+      doc.chapters!.push(chapter);
     });
   };
   epub.on('end', () => {
@@ -36,14 +46,19 @@ export function epubParse(name: string) {
         // Until the pronunciation guide.
         finished = true;
       }
-      if (finished) {
+      if (finished || !started) {
         return;
       }
       // We're good to go here.
       let docId = chapter.id.replace(/lds_([^_]+)_\d+/, '$1');
-      if (started) {
-        parseChapter(chapter.id, docId);
+      let doc = docMap.get(docId);
+      if (!doc) {
+        doc = {chapters: [], id: docId} as any as Doc;
+        // Easy lookup and maintain order.
+        docMap.set(docId, doc);
+        docs.push(doc);
       }
+      parseChapter(chapter.id, doc);
     });
     // let chapterId = 'lds_1-ne_000';
     // let chapterId = 'lds_1-ne_001';
@@ -51,9 +66,23 @@ export function epubParse(name: string) {
     // parseChapter(chapterId);
   });
   epub.parse();
+  // All parsed. Put on final touches.
+  for (let doc of docs) {
+    doc.size = sum(doc.chapters!.map(chapter => chapter.size + 1));
+  }
+  let size = sum(docs.map(doc => doc.size));
+  return {items: docs, name: 'lds_bom', size, title: 'Book of Mormon'};
 }
 
 interface EPubChapter {
   id: string;
   level?: number;
+}
+
+export function sum(numbers: Iterable<number>) {
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
 }
