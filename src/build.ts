eb32479f0186315c78c6863868477d67ca694a29
Tom <tjpalmer@tjpalmer.com>
2017-10-24 06:22:05 -0700

    Some finding

--- src/build.ts
+++ src/build.ts
@@ -11,14 +11,35 @@ async function build() {
   console.log(ldsVolume.items.length);
   ldsVolume.items.forEach((doc, docIndex) => {
     let mbDoc = mormonsBookVolume.docs[docIndex];
+    let paraIndex = -1;
+    let paraText: string;
+    let searchStart: number;
+    let nextPara = () => {
+      paraText = noPunc(mbDoc.paragraphs[++paraIndex]);
+      console.log('MBO paragraph: ', paraText);
+      searchStart = 0;
+    };
+    nextPara();
     doc.chapters!.forEach(chapter => {
       chapter.paragraphs.forEach(paragraph => {
         // Each verse in its own paragraph from lds.org.
         let verse = paragraph.verses[0];
-        let text = noPunc(verse.text);
-        console.log(text);
-        die();
+        let verseText = noPunc(verse.text);
+        console.log('LDS verse: ', verseText);
+        let verseStart = -1;
+        for (; paraIndex < mbDoc.paragraphs.length; nextPara()) {
+          verseStart = paraText.indexOf(verseText, searchStart);
+          if (verseStart >= 0) {
+            break;
+          }
+        }
+        if (verseStart >= 0) {
+          console.log(`Match at ${verseStart} in para ${paraIndex}!`);
+          searchStart = verseStart + verseText.length;
+        }
       });
+      // Just on chapter for now.
+      die();
     });
   });
 }
