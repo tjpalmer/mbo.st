e0316451d837623a6ac921f05919fedc89648f94
Tom <tjpalmer@tjpalmer.com>
2017-10-25 06:02:16 -0700

    Work toward tracking original text indices

--- src/build.ts
+++ src/build.ts
@@ -3,6 +3,11 @@ import {epubParse, mormonsBookParse} from './';
 
 build();
 
+interface Match {
+  index: number;
+  token: string;
+}
+
 async function build() {
   let ldsVolume = await epubParse('./notes/book-of-mormon-eng.epub');
   let mormonsBookVolume =
@@ -12,11 +17,11 @@ async function build() {
   ldsVolume.items.forEach((doc, docIndex) => {
     let mbDoc = mormonsBookVolume.docs[docIndex];
     let paraIndex = -1;
-    let paraText: string;
+    let paraWords: Match[];
     let searchStart: number;
     let nextPara = () => {
-      paraText = noPunc(mbDoc.paragraphs[++paraIndex]);
-      console.log('MBO paragraph: ', paraText);
+      paraWords = noPunc(mbDoc.paragraphs[++paraIndex]);
+      console.log('MBO paragraph: ', joinWords(paraWords));
       searchStart = 0;
     };
     nextPara();
@@ -24,26 +29,57 @@ async function build() {
       chapter.paragraphs.forEach(paragraph => {
         // Each verse in its own paragraph from lds.org.
         let verse = paragraph.verses[0];
-        let verseText = noPunc(verse.text);
-        console.log('LDS verse: ', verseText);
-        let verseStart = -1;
+        let verseWords = noPunc(verse.text);
+        console.log('LDS verse: ', joinWords(verseWords));
+        let end: number | undefined;
         for (; paraIndex < mbDoc.paragraphs.length; nextPara()) {
-          verseStart = paraText.indexOf(verseText, searchStart);
-          if (verseStart >= 0) {
+          end = matchWords(verseWords, paraWords, searchStart);
+          if (end) {
+            console.log('Yea!');
             break;
           }
         }
-        if (verseStart >= 0) {
-          console.log(`Match at ${verseStart} in para ${paraIndex}!`);
-          searchStart = verseStart + verseText.length;
+        if (end) {
+          let begin = paraWords[searchStart].index;
+          console.log(`Match from ${begin} to ${end} in para ${paraIndex}!`);
+          console.log(mbDoc.paragraphs[paraIndex].slice(begin, end));
+          searchStart += verseWords.length;
         }
       });
-      // Just on chapter for now.
+      // Chapter over.
       die();
     });
   });
 }
 
+function joinWords(words: Match[]) {
+  return words.map(word => word.token).join(' ');
+}
+
+function matchWords(needle: Match[], haystack: Match[], searchStart = 0) {
+  if (needle.length) {
+    let anyDiff = needle.some((word, index) => {
+      // console.log(`comparing ${word.token} and ${haystack[index]}`)
+      return word.token != haystack[index + searchStart].token;
+    });
+    if (!anyDiff) {
+      let last = haystack[searchStart + needle.length - 1];
+      return last.index + last.token.length;
+    }
+  }
+  // else undefined
+}
+
 function noPunc(text: string) {
-  return text.replace(/\W+/g, ' ').split(/\s+/).join(' ').trim();
+  let pattern = /\w+/g;
+  let i = 0;
+  let matches: Match[] = [];
+  while (true) {
+    let match = pattern.exec(text);
+    if (!match) {
+      break;
+    }
+    matches.push({index: match.index, token: match[0]});
+  }
+  return matches;
 }
